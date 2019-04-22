dd1092baeb00d1f17180ac123fbbbe9d1eeffbc6
李剑 <lijian@leadu.net.cn>
2019-04-22 14:44:02 +0800

    添加接口注释

--- spring security/spring-security-getting-started/src/main/java/com/lijian/springsecuritygettingstarted/controller/UserController.java
+++ spring security/spring-security-getting-started/src/main/java/com/lijian/springsecuritygettingstarted/controller/UserController.java
@@ -16,20 +16,34 @@ public class UserController {
     @Autowired
     private SysUserService sysUserService;
 
+    /**
+     * 用于快速初始化admin用户以及他的角色、权限，初始密码是admin
+     * @return
+     */
     @PostMapping("/admin")
     public SysUser createAdminUser(){
         return sysUserService.createAdminUser();
     }
 
+    /**
+     * 通过用户名查询用户信息
+     * @param username
+     * @return
+     */
     @GetMapping("/{username}")
     @PreAuthorize("hasAuthority('QUERY_USER_INFO')")
     public SysUser queryUser(@PathVariable String username){
         return sysUserService.queryUser(username);
     }
 
+    /**
+     * 查询个人资料
+     * @param principal
+     * @return
+     */
     @GetMapping("/self")
     public SysUser queryMySelf(Principal principal){
         return sysUserService.queryUser(principal.getName());
-//        return (SysUser) ((UsernamePasswordAuthenticationToken) principal).getPrincipal();
+//        return (SysUser) ((UsernamePasswordAuthenticationToken) principal).getPrincipal(); // 也可以通过强制转换得到
     }
 }
